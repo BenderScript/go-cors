c4d850e0f561e5544932cdc5a8d477b109beccad
repenno <rapenno@gmail.com>
2020-11-29 14:36:54 -0800

    Adding go mod

--- clean_docker.sh
+++ clean_docker.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+# One liner to stop / remove all of Docker containers:
+
+# docker stop $(docker ps -a -q)
+# docker rm $(docker ps -a -q)
+
 CONTAINER_NAME=go-cors
 
 # docker ps -a | awk '{ print $1,$2 }' | grep ${CONTAINER_NAME} | awk '{print $1 }' | xargs -I {} docker rm {}
